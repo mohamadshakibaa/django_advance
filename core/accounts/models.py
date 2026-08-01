5c15e4c333d4eba5d146cde36cd61298faf93860
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-08-01 19:09:19 +0330

    set admin model, signals

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
 )
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
@@ -44,3 +46,22 @@ class User(AbstractBaseUser,PermissionsMixin):
     
     def __str__(self):
         return self.email
+    
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.email
+    
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
