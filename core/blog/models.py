413279e9989086f0c5b59086b5b41a4c16a00f28
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-08-01 19:44:36 +0330

    add ( get_user_model ) Instead of referring to User directly.set static files and media files

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
 class Post(models.Model):
     """
     this is a claa of define posts for blog app
     """
-    author = models.ForeignKey("User", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=250)
     content = models.TextField()
--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    # 'blog',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfile",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
