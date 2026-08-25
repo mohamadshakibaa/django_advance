bc77fc56977bec0993b851ac7a2d298f368efd2b
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-08-25 16:19:32 +0330

    ModelSerializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from ...models import Post
 
 
 class PostSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     author = serializers.CharField(max_length=255)
     title = serializers.CharField(max_length=255)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["id", "title", "status", "content", "created_date", "published_date"]
