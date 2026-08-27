2e055f11fc59ff20b73b697d07b3c9aa723b0101
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-08-27 20:20:18 +0330

    Class Based View (GET, PUT, DELETE)

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'api-v1'
 
 urlpatterns = [
+    # path('post/', views.postlist, name="post-list" ),
+    # path('post/<int:id>', views.postdetail, name="post-detail" ),
     path('post/', views.PostList.as_view(), name="post-list" ),
-    path('post/<int:id>', views.postdetail, name="post-detail" ),
+    path('post/<int:id>', views.PostDetail.as_view(), name="post-detail" ),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -4,11 +4,15 @@ from .serializers import PostSerializer
 from ...models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly,
+    IsAdminUser,
+    IsAuthenticated,
+)
 from rest_framework.views import APIView
 
-
-"""
+"""Function Based View 
+                                           
 @api_view(["GET", "POST"])
 @permission_classes([IsAdminUser])
 def postlist(request):
@@ -21,36 +25,9 @@ def postlist(request):
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)"""
-    
-class PostList(APIView):
-    permission_classes = [IsAdminUser]
-    serializer_class = PostSerializer
-    
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
-"""
-@api_view()
-def postdetail(request, id):
-    # try:                                   instead of Get_Object_or_404
-        # post = Post.objects.get(pk=id)
-        post = get_object_or_404(Post, pk=id)
-        print(post.__dict__)
-        serializer = PostSerializer(post)
-        print(serializer.data)
-        return Response(serializer.data)
-    # except Post.DoesNotExist:
-        # return Response({"detail":"Post dose not exist"}, status=status.HTTP_404_NOT_FOUND)/"""
-        
+
+                                          
 @api_view(["GET", "PUT", "DELETE"])
 @permission_classes([IsAuthenticated])
 def postdetail(request, id):
@@ -66,4 +43,60 @@ def postdetail(request, id):
     if request.method == "DELETE":
         post.delete()
         return Response({"detail": "This item was deleted"}, status=status.HTTP_204_NO_CONTENT)
-        
+
+"""
+
+
+""" this part is about try/exception in Function Based View  (Not Important)                                                   
+@api_view()
+def postdetail(request, id):
+    # try:                                   instead of Get_Object_or_404
+        # post = Post.objects.get(pk=id)
+        post = get_object_or_404(Post, pk=id)
+        print(post.__dict__)
+        serializer = PostSerializer(post)
+        print(serializer.data)
+        return Response(serializer.data)
+    # except Post.DoesNotExist:
+        # return Response({"detail":"Post dose not exist"}, status=status.HTTP_404_NOT_FOUND)/
+"""
+
+
+class PostList(APIView):
+    permission_classes = [IsAdminUser]
+    serializer_class = PostSerializer
+
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class PostDetail(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get(self, request, id):
+        post = get_object_or_404(Post, pk=id)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        post = get_object_or_404(Post, pk=id)
+        serializer = PostSerializer(post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, id):
+        post = get_object_or_404(Post, pk=id)
+        post.delete()
+        return Response(
+            {"detail": "This item deleted"}, status=status.HTTP_204_NO_CONTENT
+        )
