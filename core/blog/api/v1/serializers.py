f3346a20e13fe11311279bbbc97cd2f53f0e6dfe
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-08-30 13:52:56 +0330

    feat: add custom representation for list and detail post responses
    - Add different data representations for list and single post views
    - Retrieve post pk from request.parser_context.get("kwargs").get("pk")

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -7,6 +7,12 @@ from ...models import Post, Category
 #     title = serializers.CharField(max_length=255)
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
+
+
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
@@ -16,12 +22,19 @@ class PostSerializer(serializers.ModelSerializer):
             "title",
             "status",
             "content",
+            "category",
             "created_date",
             "published_date",
         ]
 
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ["id", "name"]
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep["category"] = CategorySerializer(instance.category).data
+        request = self.context.get("request")
+        if request.parser_context.get("kwargs").get("pk"):
+            rep.pop("created_date", None)
+            rep.pop("published_date", None)
+        else:
+            rep.pop("content", None)
+            
+        return rep
