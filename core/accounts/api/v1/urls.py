d49821d7b11514c6bdec9cf992bb517538337003
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-09-06 17:11:52 +0330

    api for account

---
+++ core/accounts/api/v1/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+app_name = "api-v1"
+
+urlpatterns = [
+    path("registration/", views.RegistrationApiView.as_view(), name="registration"),
+]
--- core/core/urls.py
+++ core/core/urls.py
@@ -40,7 +40,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("", include("blog.urls")),
     path("api-auth/", include("rest_framework.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
     path("blog/", include("blog.urls")),
     path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
