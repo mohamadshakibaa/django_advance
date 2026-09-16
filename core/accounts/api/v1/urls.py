00fe728ce12f1365e4c779932fe3d5454cbed3a8
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-09-16 20:42:32 +0330

    ارسال Token فعالسازی از طریق ایمیل

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('profile/', views.ProfileApiView.as_view(), name='profile'),
     
     path('test-email/', views.TestEmailSend.as_view(), name='test-email'),
-    # path('activation/confirm/'),
+    path('activation/confirm/<str:token>', views.ActivationApiView.as_view(), name="activation"),
     # path('activation/resend/')
 ]
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -13,6 +13,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from ...models import User, Profile
 from django.shortcuts import get_object_or_404
+from mail_templated import EmailMessage
+from ..utils import CustomEmailThread
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import AuthenticationFailed
 
 
 class RegistrationApiView(generics.CreateAPIView):
@@ -22,8 +26,20 @@ class RegistrationApiView(generics.CreateAPIView):
         serializer = RegistraionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            data = {"email": serializer.validated_data("email")}
+            email = serializer.validated_data["email"]
+            data = {"email": email}
+            user_obj = get_object_or_404(User, email=email)
+            token = self.get_tokens_for_user(user_obj)
+            email_obj = EmailMessage('email/activation_email.tpl', {"token": token}, "admin@admin.com", to={email})
+            CustomEmailThread(email_obj).start()
             return Response(data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_tokens_for_user(self, user):
+    # if not user.is_active:
+    #     raise AuthenticationFailed("User is not active")
+        refresh = RefreshToken.for_user(user)
+        return str(refresh.access_token)
 
 
 class CustomObtainAuthToken(ObtainAuthToken):
@@ -37,7 +53,6 @@ class CustomObtainAuthToken(ObtainAuthToken):
         user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
         return Response({"token": token.key, "user_id": user.pk, "email": user.email})
-
 
 class CustomDiscardAuthToken(APIView):
     permission_classes = [IsAuthenticated]
@@ -84,10 +99,23 @@ from django.core.mail import send_mail
 class TestEmailSend(generics.GenericAPIView):
 
     def get(self, requset, *args, **kwargs):
-        send_mail(
-            "Subject here",
-            "Here is the message.",
-            "from@example.com",
-            ["to@example.com"],)
-        
+        email_obj = EmailMessage('email/hello.tpl', {'user': "mohamad"}, "admin@admin.com",to=["mohamad@gmail.com"])
+        CustomEmailThread(email_obj).start()
         return Response("email sent")
+    
+
+    def get_tokens_for_user(self, user):
+        # if not user.is_active:
+        #     raise AuthenticationFailed("User is not active")
+        refresh = RefreshToken.for_user(user)
+        return str(refresh.access_token)
+    
+
+class ActivationApiView(APIView):
+    def get(self, request, token, *args, **kwargs):
+        # decode
+        # object user
+        # valid
+        # or not valid
+    
+        return Response(token)
