2e362646856c24ffb1963d1c79cbd868273092d4
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2026-09-19 18:20:09 +0330

    TestCae - use SetUp in models

--- core/blog/tests/test_blog_model.py
+++ core/blog/tests/test_blog_model.py
@@ -4,16 +4,22 @@ from datetime import datetime
 from ..models import Post
 from accounts.models import Profile, User
 
+
 class TestPostModel(TestCase):
-    
+    def setUp(self):
+        # To avoid repeating it in every test, you can define it once and reuse it in any test.
+        self.user = User.objects.create_user(email="test@test.com", password="M@123456")
+
     def test_create_post_with_valid_date(self):
-        user = User.objects.create_user(email="test@test.com", password="M@123456")
+
         post = Post.objects.create(
-            author = user,
-            title ="test",
-            content ="descriptions",
-            category = None ,
-            status = True,
-            published_date = datetime.now(),
+            author=self.user,
+            title="test",
+            content="descriptions",
+            category=None,
+            status=True,
+            published_date=datetime.now(),
         )
+
         self.assertEqual(post.title, "test")
+        self.assertTrue(Post.objects.filter(pk=post.id).exists())
